1befc838d1a6f928037733321eb10da0a8e27c71
Chinh Do <chinh@doinby.co>
2023-04-01 20:27:04 +0700

    Set up tailwind

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+	return (
+		<header className='flex justify-between'>
+			<a href='/' type='text/html' className='font-header text-xl'>
+				doinby.co
+			</a>
+			<ul>
+				<li>
+					<a href='' className='text-lg'>
+						Send me an email
+					</a>
+				</li>
+			</ul>
+		</header>
+	);
+}
---
+++ src/components/Main.tsx
@@ -0,0 +1,3 @@
+export default function Main() {
+	return <main className='grow shrink-0 basis-auto'>Main</main>;
+}
