9a4e85f2c0afad97680cc85f4383b65716f5775f
Chinh Do <chinh@doinby.co>
2023-04-04 11:27:45 +0700

    Add social media icons to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,15 @@
+import { GithubWithCircle as Github } from '@styled-icons/entypo-social';
+import { LinkedinWithCircle as Linkedin } from '@styled-icons/entypo-social';
+import { TwitterWithCircle as Twitter } from '@styled-icons/entypo-social';
+
 export default function Footer() {
-	return <footer className='shrink'>Footer</footer>;
+	return (
+		<footer className='shrink'>
+			<section className='w-full flex justify-evenly'>
+				<Linkedin className='social-links' size='32' title='Github Link' />
+				<Github className='social-links' size='32' title='Github Link' />
+				<Twitter className='social-links' size='32' title='Github Link' />
+			</section>
+		</footer>
+	);
 }
