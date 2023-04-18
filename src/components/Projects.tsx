c79e4e23d4f66f6397bb91a3f029c67dd2836453
Chinh Do <chinh@doinby.co>
2023-04-18 11:56:24 +0700

    Add function to fetch and display each project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,23 @@
 import ProjectCard from './ProjectCard';
+import { repoData } from '../_data';
 
 export default function Projects() {
+	const URL = 'http://localhost:9999/.netlify/functions/fetchRepo/';
+
+	const projectCards = repoData.map((repo) => (
+		<ProjectCard
+			key={repo.slug}
+			name={repo.name}
+			url={`${URL}?owner=${repo.owner}&repo=${repo.slug}`}
+		/>
+	));
+
 	return (
-		<section className='flex flex-col gap-6 px-16 py-14 shadow-md'>
-			<h2>Projects I've worked on:</h2>
+		<section className='flex flex-col gap-6 px-16 py-14'>
+			<h2 className='font-header text-2xl text-center'>
+				Projects I've worked on:
+			</h2>
+			{projectCards}
 		</section>
 	);
 }
